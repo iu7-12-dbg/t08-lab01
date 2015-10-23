7b3502fe1ca7a50f515d6762e53406856892df69
Vladislav00 <vlad-0074@ya.ru>
2015-10-23 17:55:24 +0300

    Fixed rangecheck

--- PathfindingAlgorithms/Algorithms/Dijkstra.cs
+++ PathfindingAlgorithms/Algorithms/Dijkstra.cs
@@ -11,10 +11,10 @@ namespace PathfindingAlgorithms.Algorithms
     {
         public IEnumerable<ICell> Process(IList<IList<ICell>> CellMap, Tuple<int, int> start, Tuple<int, int> end)
         {
-            if (start.Item1 >= CellMap.Count || start.Item2 >= CellMap[start.Item1].Count)
+            if (start.Item1 < 0 || start.Item2 < 0 || start.Item1 >= CellMap.Count || start.Item2 >= CellMap[start.Item1].Count)
                 throw new ArgumentOutOfRangeException("start");
-            if (end.Item1 >= CellMap.Count || end.Item2 >= CellMap[start.Item1].Count)
-                throw new ArgumentOutOfRangeException("start");
+            if (end.Item1 < 0 || end.Item2 < 0 || end.Item1 >= CellMap.Count || end.Item2 >= CellMap[end.Item1].Count)
+                throw new ArgumentOutOfRangeException("end");
 
             throw new NotImplementedException();
         }
