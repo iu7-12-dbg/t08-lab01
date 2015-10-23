8290604a2ed5c07c6f6de1c04bcba2b63917aa44
artem.stolyarenko <roguenalan@gmail.com>
2015-10-23 17:47:30 +0300

    Added error-checking to Dijkstra algorithm

--- PathfindingAlgorithms/Algorithms/Dijkstra.cs
+++ PathfindingAlgorithms/Algorithms/Dijkstra.cs
@@ -11,7 +11,10 @@ namespace PathfindingAlgorithms.Algorithms
     {
         public IEnumerable<ICell> Process(IList<IList<ICell>> CellMap, Tuple<int, int> start, Tuple<int, int> end)
         {
-            
+            if (start.Item1 >= CellMap.Count || start.Item2 >= CellMap[start.Item1].Count)
+                throw new ArgumentOutOfRangeException("start");
+            if (end.Item1 >= CellMap.Count || end.Item2 >= CellMap[start.Item1].Count)
+                throw new ArgumentOutOfRangeException("start");
 
             throw new NotImplementedException();
         }
--- PathfindingAlgorithms/Algorithms/IPathFindingAlgorithm.cs
+++ PathfindingAlgorithms/Algorithms/IPathFindingAlgorithm.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PathfindingAlgorithms
+namespace PathfindingAlgorithms.Algorithms
 {
     public interface IPathFindingAlgorithm
     {
