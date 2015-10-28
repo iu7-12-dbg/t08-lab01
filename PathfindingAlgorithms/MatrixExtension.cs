5097de49ef873b7c029b6f4b9a28832f70cfd334
artem.stolyarenko <roguenalan@gmail.com>
2015-10-28 20:25:29 +0300

    Fixed matrix extension method

--- PathfindingAlgorithms/MatrixExtension.cs
+++ PathfindingAlgorithms/MatrixExtension.cs
@@ -9,10 +9,16 @@ namespace PathfindingAlgorithms
 {
     public static class MatrixExtension
     {
-        public static Coordinates Size<T>(this IList<IList<T>> matrix)
+        /// <summary>
+        /// Returns maximum bottom-right coordinate of given matix. Returns -1 if rows or columns are empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static Coordinates MaxCoordinate<T>(this IList<IList<T>> matrix)
         {
-            var x = matrix.Count;
-            var y = matrix[x - 1].Count;
+            var x = matrix.Count - 1;
+            var y = (x != -1) ? matrix[x].Count - 1 : -1;
             return new Coordinates(x, y);
         }
     }
