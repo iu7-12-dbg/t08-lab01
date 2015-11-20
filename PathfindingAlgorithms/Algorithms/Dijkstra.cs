830727ac7fa392ed3eb1ab783acba4eaa067a6ac
artem.stolyarenko <roguenalan@gmail.com>
2015-11-20 11:27:54 +0300

    Restored old Dijkstra version

--- PathfindingAlgorithms/Algorithms/Dijkstra.cs
+++ PathfindingAlgorithms/Algorithms/Dijkstra.cs
@@ -11,10 +11,7 @@ namespace PathfindingAlgorithms.Algorithms
     {
         public IEnumerable<ICell> Process(IList<IList<ICell>> CellMap, Tuple<int, int> start, Tuple<int, int> end)
         {
-            if (start.Item1 >= CellMap.Count || start.Item2 >= CellMap[start.Item1].Count)
-                throw new ArgumentOutOfRangeException("start");
-            if (end.Item1 >= CellMap.Count || end.Item2 >= CellMap[start.Item1].Count)
-                throw new ArgumentOutOfRangeException("start");
+            
 
             throw new NotImplementedException();
         }
