3564754c2fa982fab5eb0ac8c0632de54876466c
artem.stolyarenko <roguenalan@gmail.com>
2015-11-24 17:52:42 +0300

    Added ValueCell

---
+++ PathfindingAlgorithms/Cells/ValueCell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfindingAlgorithms.Cells
+{
+    public class ValueCell : ICell
+    {
+        public ValueCell(Coordinates coordinates)
+            : this(coordinates, 0)
+        {
+        }
+
+        public ValueCell(Coordinates coordinates, double weight)
+        {
+            this.Coordinates = coordinates;
+            this.Weight = weight;
+        }
+
+        public Coordinates Coordinates { get; }
+
+        public double Weight { get; set; }
+    }
+}
