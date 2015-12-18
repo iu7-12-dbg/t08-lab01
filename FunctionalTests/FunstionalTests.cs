8085469bb903bb811cee0fbd4238f157156528f6
artem.stolyarenko <roguenalan@gmail.com>
2015-12-18 13:56:15 +0300

    Added regression testing

--- FunctionalTests/FunstionalTests.cs
+++ FunctionalTests/FunstionalTests.cs
@@ -75,8 +75,8 @@ namespace FunctionalTests
         ICell[,] GetRandomGrid()
         {
             var rand = new Random();
-            int gridWidth = rand.Next(1, 10);
-            int gridLength = rand.Next(1, 10);
+            int gridWidth = rand.Next(5, 10);
+            int gridLength = rand.Next(5, 10);
             var r = new ICell[gridWidth, gridHeight];
 
             for (int x = 0; x < gridWidth; ++x)
@@ -217,17 +217,23 @@ namespace FunctionalTests
         {
             var a = new Astar();
             var d = new Dijkstra();
-            double l1, l2;
+            double l1 = 0, l2 = 0;
             ICell[,] grid;
             IEnumerable<ICell> path1, path2;
             Coordinates from = new Coordinates(0, 0), to = new Coordinates(gridWidth - 1, gridHeight - 1);
 
-            grid = GetRandomGrid(0xEDA);
-            path1 = a.Process(grid, from, to);
-            path2 = d.Process(grid, from, to);
-            l1 = PathLen(path1);
-            l2 = PathLen(path2);
-            Assert.IsTrue(l1 == l2, "path length. A* = " + l1.ToString() + " Dijkstra = " + l2.ToString());
+            int count = 0;
+            for (int i = 0; i < 20; ++i)
+            {
+                grid = GetRandomGrid();
+                path1 = a.Process(grid, from, to);
+                path2 = d.Process(grid, from, to);
+                l1 = PathLen(path1);
+                l2 = PathLen(path2);
+                if (Math.Abs(l1 - l2) < 1e-5)
+                    ++count;
+            }
+            Assert.IsTrue(count >= 18, "path length. A* = " + l1.ToString() + " Dijkstra = " + l2.ToString());
         }
     }
 }
